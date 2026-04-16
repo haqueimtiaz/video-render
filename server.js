054f69319598120d5c813703ab497e87e90bdeb2
haqueimtiaz <haqueimtiaz@gmail.com>
2026-04-17 00:35:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const https = require("https");
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Video renderer running");
